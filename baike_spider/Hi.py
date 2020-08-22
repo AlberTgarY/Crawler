235c4f870482737adc4e3a4330dda495c59e771a
Albert <43678364+AlberTgarY@users.noreply.github.com>
2020-08-23 01:33:30 +0800

    Update Hi.py

--- baike_spider/Hi.py
+++ baike_spider/Hi.py
@@ -1,15 +1,46 @@
-# s = '国'
-#
-# s2 = s.encode('utf-8')
-# for n in range(1, 5):
-#     print(n)
-# b = 'ÈË²ÎÓëÌÖÂÛÀ´Ô´ >  ÍøÒ×Êý¶ÁÓÖµ½ÁË¸ßÈý±ÏÒµÉúÐÄÇé×îìþìýµÄÊ±ºòÁË£¬´Ó2020Äê7ÔÂ23ÈÕ¿ªÊ¼£¬Â½'
-# e = bytes(b,encoding='utf-8')
-# c = e.decode('utf-8')
-# print(c)
-from fake_useragent import UserAgent
-ua = UserAgent()
+import html_downloader
+from bs4 import BeautifulSoup
 
-#最常用的方式
-#写爬虫最实用的是可以随意变换headers，一定要有随机性。支持随机生成请求头
-print(ua.random)
+
+# downloader = html_downloader.HtmlDownloader()
+# html_cont = downloader.download("https://api.slack.com/events-api")
+# soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+# news_content = soup.find_all('table', {'class': 'table table-bordered full_width'})
+#
+# tab = news_content[0]
+# dictList = []
+# for tr in tab.findAll('tr'):
+#     list = [('name', ""), ('description', ""), ('scope', "")]
+#     count = 0
+#     for td in tr.findAll('td'):
+#         if count == 0:
+#             list[0] = ('name', td.getText())
+#         elif count == 1:
+#             list[1] = ('description', td.getText())
+#         else:
+#             list[2] = ('scope', td.getText())
+#         count = count + 1
+#     dic = dict(list)
+#     dictList.append(dic)
+downloader = html_downloader.HtmlDownloader()
+html_cont = downloader.download("https://api.slack.com/methods")
+soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+news_content = soup.find_all('div', {'class': 'tab_pane selected'})
+
+tab = news_content[0]
+dictList = []
+for tr in tab.findAll('tr'):
+    list = [('name', ""), ('description', "")]
+    count = 0
+    for td in tr.findAll('a',href = True):
+        print(td['href'])
+
+    #     if count == 0:
+    #         list[0] = ('name', td.getText())
+    #     elif count == 1:
+    #         list[1] = ('description', td.getText())
+    #     count = count + 1
+    # dic = dict(list)
+    # dictList.append(dic)
+print()
+print(dictList)
