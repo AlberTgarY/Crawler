fe3fd62bdbe27105df6e88d90af4655bb6f0ba60
Albert <43678364+AlberTgarY@users.noreply.github.com>
2020-09-04 23:45:56 +0800

    Update html_downloader.py

--- baike_spider/src/html_downloader.py
+++ baike_spider/src/html_downloader.py
@@ -1,58 +1,81 @@
 # coding=utf-8
-import ssl
-import configparser
-from urllib import request
-from fake_useragent import UserAgent
-from Logger import get_log
 
-# 读取配置文件
+# HTML 输出器
+
+import pandas as pd
+from Logger import get_log
+import configparser
+import datetime
+import os
+import openpyxl
+
+logger = get_log()
 config = configparser.RawConfigParser()
 config.read("./temp/cfg.ini")
-INVALID_URL_TXT_PATH = str(config.get("path", "txt_path"))
 
-class HtmlDownloader(object):
 
-    #   下载当前页，返回下载信息
-    def download(self, url):
-        logger = get_log()
-        context = ssl._create_unverified_context()
-        if url is None:
-            raise ValueError('target url is None')
-        if url[0:2] == '//' and url[2:5] == 'www':
-            url = "http:"+url
+def export_excel(export):
+    try:
+        time = str(datetime.datetime.now().year)+"-"+str(datetime.datetime.now().month)+"-"+str(datetime.datetime.now().day)
+        EXCEL_PATH = str(config.get("path", "excel_path"))
+        filename = EXCEL_PATH+time+".xlsx"
+        # 指定字段顺序
+        order = ['URL', 'Website', 'Type', 'Title', 'Value']
+        # 将字典列表转换为DataFrame
+        pf = pd.DataFrame(list(export))
+        pf = pf[order]
+        if not os.path.exists(filename):
+            writer = pd.ExcelWriter(filename, mode='w')
+            # 替换空单元格
+            pf.fillna(' ', inplace=True)
+            # 输出
+            pf.to_excel(writer, encoding='utf-8', index=False, sheet_name='d1')
+            # 保存表格
+            writer.save()
+            writer.close()
+            logger.info("Excel file " + filename + " created")
 
+        else:
+            writer = pd.ExcelWriter(filename, mode='a', engine='openpyxl')
+            writer.book = openpyxl.load_workbook(filename)
+            # 替换空单元格
+            pf.fillna(' ', inplace=True)
+            # 输出
+            pf.to_excel(writer, encoding='utf-8', index=False, sheet_name='d2')
+            # 保存表格
+            writer.save()
+            writer.close()
+            logger.info("Excel file " + filename + " exists and news have been added")
+
+
+    except Exception as e:
+        print(e)
+
+
+class HtmlOutputer(object):
+
+    # 初始化数据集为空
+    def __init__(self):
+
+        self.datas = []
+
+    # 收集数据用来最后输出
+    def collect_data(self, data):
+
+        if data is None:
+            return
+        self.datas.append(data)
+
+    # 按照 excel 格式输出
+    def output_excel(self):
+        dict_list = []
+        # construct the dict to a certain format
         try:
-            ua = UserAgent()
-            # fake headers
-            headers = {"User-Agent": ua.random}
-            # request website
-            r = request.Request(url=url, headers=headers)
-            # r1 = requests.get(url=url, headers=headers, timeout=15)
-            logger.info("urlopen: " + url)
-            response = request.urlopen(r, context=context, timeout=15)
-
-            # print(str(response.info()["Content-Type"]))
-            if response.getcode() != 200:
-                logger.info("url code: " + response.getcode())
-                return None
-            elif str(response.info()["Content-Type"]).find("text/html") == -1:
-                if str(response.info()["Content-Type"]).find("text/xml") != -1:
-                    logger.info("Read data successfully")
-                    return response.read()
-                raise Exception("The url type is not HTML")
-            else:
-                logger.info("Read data successfully")
-                return response.read()
+            for data in self.datas:
+                for Type, key, value, website in zip(data['type'].values(), data['summary'].keys(),
+                                                     data['summary'].values(), data['website'].values()):
+                    temp_dict = {'URL': data['url'], "Website": website, "Type": Type, "Title": key, "Value": value}
+                    dict_list.append(temp_dict)
         except Exception as e:
-            logger.info("Got a error while downloading: " + str(e))
-            print(url+' got a error: [' + str(e) + "]")
-            # if str(e) == 'HTTP Error 404: Not Found' or str(e) == 'The url type is not HTML':
-            with open(INVALID_URL_TXT_PATH, 'a') as object:
-                object.write(url+"\n")
-                logger.info(url + "has been recorded")
-                print(url+" has been recorded")
-                return None
-
-
-
-
+            print(e)
+        export_excel(dict_list)
